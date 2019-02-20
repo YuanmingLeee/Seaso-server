602026c40e23320eaee5c3612bc866dddee151a5
YuanmingLeee <YLI056@e.ntu.edu.sg>
2019-02-20 11:29:40 +0800

    Update Entity
    Create Comment

---
+++ src/main/java/com/seaso/seaso/modules/comment/entity/Comment.java
@@ -0,0 +1,101 @@
+package com.seaso.seaso.modules.comment.entity;
+
+import com.seaso.seaso.common.persistance.DataEntity;
+import com.seaso.seaso.modules.answer.entity.Answer;
+
+import javax.persistence.*;
+
+/**
+ * Comment Entity class is mapped to COMMENT table.
+ *
+ * @author Yuanming Li
+ * @version 0.1
+ */
+@Entity
+@Table(name = "comment",
+        indexes = {@Index(name = "comment_comment_id_uindex", columnList = "commentId", unique = true)})
+public class Comment extends DataEntity<Comment> {
+
+    /**
+     * Id for primary key of persistence layer
+     */
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * Comment id
+     */
+    @Column(nullable = false, unique = true, length = 16)
+    private String commentId;
+
+    /**
+     * Answer id which this comment replies
+     */
+    @ManyToOne
+    @JoinColumn(name = "answerId", referencedColumnName = "answerId", nullable = false,
+            foreignKey = @ForeignKey(name = "comment_answer_answer_id_fk"))
+    private Answer answer;
+
+    /**
+     * Replied comment id which this comment replies
+     */
+    @ManyToOne
+    @JoinColumn(name = "replyId", referencedColumnName = "commentId",
+            foreignKey = @ForeignKey(name = "comment_comment_id_fk"))
+    private Comment replyId;
+
+    /**
+     * Comment content
+     */
+    @Basic(fetch = FetchType.LAZY)
+    @Column(length = 240)
+    private String content = "";
+
+    /**
+     * Default constructor
+     */
+    public Comment() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(String commentId) {
+        this.commentId = commentId;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+
+    public Comment getReplyId() {
+        return replyId;
+    }
+
+    public void setReplyId(Comment replyId) {
+        this.replyId = replyId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
