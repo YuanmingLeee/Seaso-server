8a0c39300572ef9337321337ae501dc070386072
YuanmingLeee <YLI056@e.ntu.edu.sg>
2019-03-20 23:54:21 +0800

    update security
    add basic authentication

---
+++ src/main/java/com/seaso/seaso/common/security/CustomFilter.java
@@ -0,0 +1,16 @@
+package com.seaso.seaso.common.security;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class CustomFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
